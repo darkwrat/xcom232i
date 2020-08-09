2bdcda3aa9100c0b1c61651dacdcfdefdacde907
Maxim Galaganov <m.galaganov@corp.mail.ru>
2020-08-09 18:06:44 +0300

    Lua formatting

--- xci.lua
+++ xci.lua
@@ -11,20 +11,56 @@ function xci_read_software_version(xp, dst_addr, msb_object_id, lsb_object_id)
 end
 
 function xci_print_info(xp)
-	print(string.format('Ubat = %.2f V (Battery voltage)', xcic.unpack_le_float(xp:read_user_info(101, 3000))))
-	print(string.format('SOC = %.2f %% (State of charge)', xcic.unpack_le_float(xp:read_user_info(101, 3007))))
-	print(string.format('Tbat = %.2f C (Battery temperature)', xcic.unpack_le_float(xp:read_user_info(101, 3001))))
-	print(string.format('U in = %.2f V (Input voltage)', xcic.unpack_le_float(xp:read_user_info(101, 3011))))
-	print(string.format('U out = %.2f V (Output voltage)', xcic.unpack_le_float(xp:read_user_info(101, 3021))))
-	print(string.format('I in = %.2f A (Input current)', xcic.unpack_le_float(xp:read_user_info(101, 3012))))
-	print(string.format('I out = %.2f A (Output current)', xcic.unpack_le_float(xp:read_user_info(101, 3022))))
-	print(string.format('PVxP = %.2f kW (Max power production for the current day)', xcic.unpack_le_float(xp:read_user_info(301, 11019))))
-	print(string.format('Psol = %.2f kW (Power of the PV generator)', xcic.unpack_le_float(xp:read_user_info(301, 11004))))
-	print(string.format('Upv = %.2f V (Voltage of the PV generator)', xcic.unpack_le_float(xp:read_user_info(301, 11002))))
-	print(string.format('Ed = %.2f kWh (Production in (kWh) for the current day)', xcic.unpack_le_float(xp:read_user_info(301, 11007))))
+	-- xtender
+	print(string.format('Ubat = %.2f V (Battery voltage)',
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3000)
+		)))
+	print(string.format('SOC = %.2f %% (State of charge)',
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3007)
+		)))
+	print(string.format('Tbat = %.2f C (Battery temperature)',
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3001)
+		)))
+	print(string.format('U in = %.2f V (Input voltage)',
+		xcic.unpack_le_float(xp:read_user_info(101, 3011)
+		)))
+	print(string.format('U out = %.2f V (Output voltage)',
+		xcic.unpack_le_float(xp:read_user_info(101, 3021)
+		)))
+	print(string.format('I in = %.2f A (Input current)',
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3012)
+		)))
+	print(string.format('I out = %.2f A (Output current)',
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3022)
+		)))
+
+	-- variotrack
+	print(string.format('PVxP = %.2f kW (Max power production for the current day)',
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11019)
+		)))
+	print(string.format('Psol = %.2f kW (Power of the PV generator)',
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11004)
+		)))
+	print(string.format('Upv = %.2f V (Voltage of the PV generator)',
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11002)
+		)))
+	print(string.format('Ed = %.2f kWh (Production in (kWh) for the current day)',
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11007)
+		)))
 end
 
 local xp = xcic.open_port('/dev/ttyS0')
+
+print('----')
 
 print(string.format('VarioTrack software version = %s', xci_read_software_version(xp, 301, 11050, 11051)))
 print(string.format('BSP software version = %s', xci_read_software_version(xp, 601, 7037, 7038)))
