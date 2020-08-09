df9a373a3eaf2eb6339cc49a53fd19f692acba15
Maxim Galaganov <m.galaganov@corp.mail.ru>
2020-08-09 19:45:00 +0300

    Prometheus export

--- xci.lua
+++ xci.lua
@@ -3,101 +3,100 @@
 require('strict').on()
 
 local xcic = require('xcic')
+local metrics = require('metrics')
 
-function xci_read_software_version(xp, dst_addr, msb_object_id, lsb_object_id)
-	return xcic.unpack_software_version(
-		xp:read_user_info(dst_addr, msb_object_id),
-		xp:read_user_info(dst_addr, lsb_object_id));
-end
-
-function xci_print_info(xp)
-	-- xtender
-	print(string.format('Ubat = %.2f V (Battery voltage)',
-		xcic.unpack_le_float(
-			xp:read_user_info(101, 3000)
-		)))
-	print(string.format('SOC = %.2f %% (State of charge)',
-		xcic.unpack_le_float(
-			xp:read_user_info(101, 3007)
-		)))
-	print(string.format('Tbat = %.2f C (Battery temperature)',
-		xcic.unpack_le_float(
-			xp:read_user_info(101, 3001)
-		)))
-	print(string.format('U in = %.2f V (Input voltage)',
-		xcic.unpack_le_float(xp:read_user_info(101, 3011)
-		)))
-	print(string.format('U out = %.2f V (Output voltage)',
-		xcic.unpack_le_float(xp:read_user_info(101, 3021)
-		)))
-	print(string.format('I in = %.2f A (Input current)',
-		xcic.unpack_le_float(
-			xp:read_user_info(101, 3012)
-		)))
-	print(string.format('I out = %.2f A (Output current)',
-		xcic.unpack_le_float(
-			xp:read_user_info(101, 3022)
-		)))
-	print(string.format('F in = %.2f Hz (Input frequency)',
-		xcic.unpack_le_float(
-			xp:read_user_info(101, 3014)
-		)))
-	print(string.format('F out = %.2f Hz (Output Frequency)',
-		xcic.unpack_le_float(
-			xp:read_user_info(101, 3024)
-		)))
-
-	-- variotrack
-	print(string.format('PVxP = %.2f kW (Max power production for the current day)',
-		xcic.unpack_le_float(
-			xp:read_user_info(301, 11019)
-		)))
-	print(string.format('Psol = %.2f kW (Power of the PV generator)',
-		xcic.unpack_le_float(
-			xp:read_user_info(301, 11004)
-		)))
-	print(string.format('Upv = %.2f V (Voltage of the PV generator)',
-		xcic.unpack_le_float(
-			xp:read_user_info(301, 11002)
-		)))
-	print(string.format('Ed = %.2f kWh (Production in (kWh) for the current day)',
-		xcic.unpack_le_float(
-			xp:read_user_info(301, 11007)
-		)))
-end
+local http_router = require('http.router').new()
+local http_handler = require('metrics.plugins.prometheus').collect_http
+local http_server = require('http.server').new('0.0.0.0', 8088)
 
 local xp = xcic.open_port('/dev/ttyS0')
 
-print('----')
+local xci_xt_battery_voltage_gauge =
+	metrics.gauge('xci_xt_battery_voltage')
+local xci_xt_battery_soc_gauge =
+	metrics.gauge('xci_xt_battery_soc')
+local xci_xt_battery_temperature_gauge =
+	metrics.gauge('xci_xt_battery_temperature')
+local xci_xt_input_voltage_gauge =
+	metrics.gauge('xci_xt_input_voltage')
+local xci_xt_output_voltage_gauge =
+	metrics.gauge('xci_xt_output_voltage')
+local xci_xt_input_current_gauge =
+	metrics.gauge('xci_xt_input_current')
+local xci_xt_output_current_gauge =
+	metrics.gauge('xci_xt_output_current')
+local xci_xt_input_frequency_gauge =
+	metrics.gauge('xci_xt_input_frequency')
+local xci_xt_output_frequency_gauge =
+	metrics.gauge('xci_xt_output_frequency')
 
-print(string.format('VarioTrack software version = %s', xci_read_software_version(xp, 301, 11050, 11051)))
-print(string.format('BSP software version = %s', xci_read_software_version(xp, 601, 7037, 7038)))
+local xci_vt_ed_gauge =
+	metrics.gauge('xci_vt_ed')
+local xci_vt_pvxp_gauge =
+	metrics.gauge('xci_vt_pvxp')
+local xci_vt_psol_gauge =
+	metrics.gauge('xci_vt_psol')
+local xci_vt_upv_gauge =
+	metrics.gauge('xci_vt_upv')
 
-print('----')
+metrics.register_callback(function()
+	-- xtender
+	xci_xt_battery_voltage_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3000)
+		))
+	xci_xt_battery_soc_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3007)
+		))
+	xci_xt_battery_temperature_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3001)
+		))
+	xci_xt_input_voltage_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3011)
+		))
+	xci_xt_output_voltage_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3021)
+		))
+	xci_xt_input_current_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3012)
+		))
+	xci_xt_output_current_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3022)
+		))
+	xci_xt_input_frequency_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3014)
+		))
+	xci_xt_output_frequency_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3024)
+		))
 
-xci_print_info(xp)
+	-- variotrack
+	xci_vt_ed_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11007)
+		))
+	xci_vt_pvxp_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11019)
+		))
+	xci_vt_psol_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11004)
+		))
+	xci_vt_upv_gauge:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11002)
+		))
+end)
 
-print('----')
-
-print(string.format('VALUE = %.2f Vdc (Battery overvoltage level)',
-	xcic.unpack_le_float(
-		xp:read_parameter_property(101, 1121, xcic.PARAMETER_PROPERTY_VALUE_QSP)
-	)))
-print(string.format('MIN = %.2f Vdc (Battery overvoltage level)',
-	xcic.unpack_le_float(
-		xp:read_parameter_property(101, 1121, xcic.PARAMETER_PROPERTY_MIN_QSP)
-	)))
-print(string.format('MAX = %.2f Vdc (Battery overvoltage level)',
-	xcic.unpack_le_float(
-		xp:read_parameter_property(101, 1121, xcic.PARAMETER_PROPERTY_MAX_QSP)
-	)))
-print(string.format('LEVEL = 0x%x (Battery overvoltage level)',
-	xcic.unpack_le16(
-		xp:read_parameter_property(101, 1121, xcic.PARAMETER_PROPERTY_LEVEL_QSP)
-	)))
---print(string.format('UNSAVED_VALUE = %.2f Vdc (Battery overvoltage level)',
---	xcic.unpack_le_float(
---		xp:read_parameter_property(101, 1121, xcic.PARAMETER_PROPERTY_UNSAVED_VALUE_QSP)
---	)))
-
-print('----')
+http_server:set_router(http_router)
+http_router:route({path = '/metrics'}, function(...) return http_handler(...) end)
+http_server:start()
