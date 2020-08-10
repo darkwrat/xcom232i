443b500a3ac45f07c16ff4517676a3f9579fd455
Maxim Galaganov <m.galaganov@corp.mail.ru>
2020-08-10 23:38:21 +0300

    Metatables

--- xci.lua
+++ xci.lua
@@ -9,101 +9,100 @@ local http_server = require('http.server').new('0.0.0.0', 8088)
 
 xp = xcic.open_port('/dev/ttyS0')
 
-local xci_xt_battery_voltage_gauge =
-	metrics.gauge('xci_xt_battery_voltage')
-local xci_xt_battery_soc_gauge =
-	metrics.gauge('xci_xt_battery_soc')
-local xci_xt_battery_temperature_gauge =
-	metrics.gauge('xci_xt_battery_temperature')
-local xci_xt_input_voltage_gauge =
-	metrics.gauge('xci_xt_input_voltage')
-local xci_xt_output_voltage_gauge =
-	metrics.gauge('xci_xt_output_voltage')
-local xci_xt_input_current_gauge =
-	metrics.gauge('xci_xt_input_current')
-local xci_xt_output_current_gauge =
-	metrics.gauge('xci_xt_output_current')
-local xci_xt_input_frequency_gauge =
-	metrics.gauge('xci_xt_input_frequency')
-local xci_xt_output_frequency_gauge =
-	metrics.gauge('xci_xt_output_frequency')
-
-local xci_vt_ed_gauge =
-	metrics.gauge('xci_vt_ed')
-local xci_vt_pvxp_gauge =
-	metrics.gauge('xci_vt_pvxp')
-local xci_vt_psol_gauge =
-	metrics.gauge('xci_vt_psol')
-local xci_vt_upv_gauge =
-	metrics.gauge('xci_vt_upv')
-
-local function xci_metrics_callback()
+local function xci_metric_callback(self)
 	-- xtender
-	xci_xt_battery_voltage_gauge:set(
+	self.gauge.xt_battery_voltage:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3000)
 		))
-	xci_xt_battery_soc_gauge:set(
+	self.gauge.xt_battery_soc:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3007)
 		))
-	xci_xt_battery_temperature_gauge:set(
+	self.gauge.xt_battery_temperature:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3001)
 		))
-	xci_xt_input_voltage_gauge:set(
+	self.gauge.xt_input_voltage:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3011)
 		))
-	xci_xt_output_voltage_gauge:set(
+	self.gauge.xt_output_voltage:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3021)
 		))
-	xci_xt_input_current_gauge:set(
+	self.gauge.xt_input_current:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3012)
 		))
-	xci_xt_output_current_gauge:set(
+	self.gauge.xt_output_current:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3022)
 		))
-	xci_xt_input_frequency_gauge:set(
+	self.gauge.xt_input_frequency:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3014)
 		))
-	xci_xt_output_frequency_gauge:set(
+	self.gauge.xt_output_frequency:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3024)
 		))
 
 	-- variotrack
-	xci_vt_ed_gauge:set(
+	self.gauge.vt_ed:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(301, 11007)
 		))
-	xci_vt_pvxp_gauge:set(
+	self.gauge.vt_pvxp:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(301, 11019)
 		))
-	xci_vt_psol_gauge:set(
+	self.gauge.vt_psol:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(301, 11004)
 		))
-	xci_vt_upv_gauge:set(
+	self.gauge.vt_upv:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(301, 11002)
 		))
 end
 
-local function xci_start()
-	metrics.register_callback(xci_metrics_callback)
-
-	http_server:set_router(http_router)
-	http_router:route({path = '/metrics'}, function(...) return http_handler(...) end)
-	http_server:start()
-end
-
+local xci_metric = {
+	counter = setmetatable({}, {
+		__index = function(self, k)
+			local cc = rawget(self, k)
+			if cc == nil then
+				cc = metrics.counter('xci_' .. k)
+				self.k = cc
+			end
+			return cc
+		end}),
+	gauge = setmetatable({}, {
+		__index = function(self, k)
+			local gg = rawget(self, k)
+			if gg == nil then
+				gg = metrics.gauge('xci_' .. k)
+				self.k = gg
+			end
+			return gg
+		end}),
+	histogram = setmetatable({}, {
+		__index = function(self, k)
+			local hh = rawget(self, k)
+			if hh == nil then
+				hh = metrics.histogram('xci_' .. k)
+				self.k = hh
+			end
+			return hh
+		end}),
+}
 
 return {
-	start = xci_start
+	start = function()
+		metrics.register_callback(setmetatable(xci_metric, {__call = xci_metric_callback}))
+
+		http_server:set_router(http_router)
+		http_router:route({path = '/metrics'}, function(...) return http_handler(...) end)
+		http_server:start()
+	end
 }
