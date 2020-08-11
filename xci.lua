0aae1a153602a81500a96c5049265c4eb2b7ca12
Maxim Galaganov <m.galaganov@corp.mail.ru>
2020-08-12 00:48:24 +0300

    Read all infos from csv

--- xci.lua
+++ xci.lua
@@ -44,6 +44,86 @@ local function xci_metric_callback(self)
 		xcic.unpack_le_float(
 			xp:read_user_info(101, 3024)
 		))
+	self.gauge.xt_input_power:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3138)
+		))
+	self.gauge.xt_output_power:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3139)
+		))
+	self.gauge.xt_ubat_min:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3090)
+		))
+	self.gauge.xt_uin:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3113)
+		))
+	self.gauge.xt_iin:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3116)
+		))
+	self.gauge.xt_pout:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3098)
+		))
+	self.gauge.xt_pout_plus:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3097)
+		))
+	self.gauge.xt_fout:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3110)
+		))
+	self.gauge.xt_fin:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3122)
+		))
+	self.gauge.xt_phase:set(
+		xcic.unpack_le16(
+			xp:read_user_info(101, 3010)
+		))
+	self.gauge.xt_mode:set(
+		xcic.unpack_le16(
+			xp:read_user_info(101, 3028)
+		))
+	self.gauge.xt_transfert:set(
+		xcic.unpack_le16(
+			xp:read_user_info(101, 3020)
+		))
+	self.gauge.xt_rme:set(
+		xcic.unpack_le16(
+			xp:read_user_info(101, 3086)
+		))
+	self.gauge.xt_aux1:set(
+		xcic.unpack_le16(
+			xp:read_user_info(101, 3054)
+		))
+	self.gauge.xt_aux2:set(
+		xcic.unpack_le16(
+			xp:read_user_info(101, 3055)
+		))
+	self.gauge.xt_ubat:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3092)
+		))
+	self.gauge.xt_ibat:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3095)
+		))
+	self.gauge.xt_pin_a:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3119)
+		))
+	self.gauge.xt_pout_a:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3101)
+		))
+	self.gauge.xt_dev1_plus:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(101, 3103)
+		))
 
 	-- variotrack
 	self.gauge.vt_ed:set(
@@ -61,6 +141,64 @@ local function xci_metric_callback(self)
 	self.gauge.vt_upv:set(
 		xcic.unpack_le_float(
 			xp:read_user_info(301, 11002)
+		))
+	self.gauge.vt_psom:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11043)
+		))
+	self.gauge.vt_mode:set(
+		xcic.unpack_le16(
+			xp:read_user_info(301, 11016)
+		))
+	self.gauge.vt_dev1:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11045)
+		))
+	self.gauge.vt_upvm:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11041)
+		))
+	self.gauge.vt_ibam:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11040)
+		))
+	self.gauge.vt_ubam:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(301, 11039)
+		))
+	self.gauge.vt_phas:set(
+		xcic.unpack_le16(
+			xp:read_user_info(301, 11038)
+		))
+	self.gauge.vt_rme:set(
+		xcic.unpack_le16(
+			xp:read_user_info(301, 11082)
+		))
+	self.gauge.vt_aux1:set(
+		xcic.unpack_le16(
+			xp:read_user_info(301, 11061)
+		))
+	self.gauge.vt_aux2:set(
+		xcic.unpack_le16(
+			xp:read_user_info(301, 11062)
+		))
+
+	-- bsp
+	self.gauge.bsp_ubat:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(601, 7030)
+		))
+	self.gauge.bsp_ibat:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(601, 7031)
+		))
+	self.gauge.bsp_soc:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(601, 7032)
+		))
+	self.gauge.bsp_tbat:set(
+		xcic.unpack_le_float(
+			xp:read_user_info(601, 7033)
 		))
 end
 
