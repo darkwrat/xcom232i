aebe3af5f7dd8896558cd529de3a188d5e96420c
Maxim Galaganov <m.galaganov@corp.mail.ru>
2020-08-29 01:19:06 +0300

    Xtender version is ok now

--- scripts/version.lua
+++ scripts/version.lua
@@ -7,7 +7,7 @@ local function xci_read_software_version(xp, dst_addr, msb_object_id, lsb_object
 end
 
 return {
-	--xtender = xci_read_software_version(xp, 101, 3130, 3131),
+	xtender = xci_read_software_version(xp, 101, 3130, 3131),
 	variotrack = xci_read_software_version(xp, 301, 11050, 11051),
 	bsp = xci_read_software_version(xp, 601, 7037, 7038),
 }
