f3a80236a0a890171832664934528884cf516abf
Maxim Galaganov <m.galaganov@corp.mail.ru>
2020-08-29 11:08:24 +0300

    Use unsaved value in limits script

--- scripts/limits.lua
+++ scripts/limits.lua
@@ -3,13 +3,13 @@ require('strict').on()
 return {
 	frequency = {
 		over_hz = xcic.unpack_le_float(
-			xp:read_parameter_property(101, 1505, 0x5)
+			xp:read_parameter_property(101, 1505, 0xD)
 			),
 		under_hz = xcic.unpack_le_float(
-			xp:read_parameter_property(101, 1506, 0x5)
+			xp:read_parameter_property(101, 1506, 0xD)
 			),
 		delay_before_transfer_sec = xcic.unpack_le_float(
-			xp:read_parameter_property(101, 1507, 0x5)
+			xp:read_parameter_property(101, 1507, 0xD)
 			),
 	}
 }
