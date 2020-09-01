e2960462453ace8b2c14d2cd778118ad9c1a5183
Maxim Galaganov <m.galaganov@corp.mail.ru>
2020-09-01 14:07:50 +0300

    Limits script enhancements

--- scripts/limits.lua
+++ scripts/limits.lua
@@ -1,6 +1,33 @@
 require('strict').on()
 
 return {
+	transfer = {
+		allowed = xp.unpack_bool(
+			xp():read_parameter_property(101, 1128, 0xD)
+			),
+		delay = {
+			voltage_open_sec = xp.unpack_le_float(
+				xp():read_parameter_property(101, 1198, 0xD)
+				),
+			frequency_open_sec = xp.unpack_le_float(
+				xp():read_parameter_property(101, 1507, 0xD)
+				),
+			close_sec = xp.unpack_le_float(
+				xp():read_parameter_property(101, 1580, 0xD)
+				),
+		},
+	},
+	voltage = {
+		under_vac = xp.unpack_le_float(
+			xp():read_parameter_property(101, 1199, 0xD)
+			),
+		under_immediate_vac = xp.unpack_le_float(
+			xp():read_parameter_property(101, 1200, 0xD)
+			),
+		absolute_max_vac = xp.unpack_le_float(
+			xp():read_parameter_property(101, 1432, 0xD)
+			),
+	},
 	frequency = {
 		over_hz = xp.unpack_le_float(
 			xp():read_parameter_property(101, 1505, 0xD)
@@ -8,8 +35,5 @@ return {
 		under_hz = xp.unpack_le_float(
 			xp():read_parameter_property(101, 1506, 0xD)
 			),
-		delay_before_transfer_sec = xp.unpack_le_float(
-			xp():read_parameter_property(101, 1507, 0xD)
-			),
-	}
+	},
 }
